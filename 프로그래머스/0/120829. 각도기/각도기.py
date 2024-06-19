9d9c7d16eb56926c10fec7ef3eaccc899ba29980
taeyoung KIM <135773366+xaeyoungkim@users.noreply.github.com>
2024-06-19 22:48:17 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -0,0 +1,14 @@
+def solution(angle):
+    if (0<angle<90):
+        answer = 1
+        return answer
+    elif(angle==90):
+        answer = 2
+        return answer
+    elif(90<angle<180):
+        answer = 3
+        return answer
+    elif(angle==180):
+        answer = 4
+        return answer
+        
